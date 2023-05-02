73cbaca09eca16d37a16d740ed5377be37e18722
张存夏 <252946169@qq.com>
2023-05-02 10:14:46 +0800

    feat: 调通增删改

--- src/pages/ConfigList/api.js
+++ src/pages/ConfigList/api.js
@@ -5,13 +5,13 @@ export async function getConfig(params) {
 }
 
 export async function postSave(data) {
-  return axios.post("/job/save", data);
+  return axios.post("/api/job/save", data);
 }
 
 export async function postUpdate(data) {
-  return axios.post("/job/update", data);
+  return axios.post("/api/job/update", data);
 }
 
 export async function postDelete(data) {
-  return axios.post("/job/delete", data);
+  return axios.post("/api/job/delete", data);
 }
